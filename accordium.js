93aa9c78947e15b1a5c593ff11e1e2283f374232
Gabriel <gabriel.batista@iesde.com.br>
2023-11-30 17:57:33 -0300

    wip: creating modes functions

--- accordium.js
+++ accordium.js
@@ -16,15 +16,35 @@ class Accordium {
     
         for (let i = 0; i < this.accordions.length; i++) {
             const headers = this.accordions[i].querySelectorAll('[data-accordion-header]');
-    
+            const contents = this.accordions[i].querySelectorAll('[data-accordion-content]');
+            
             for (let j = 0; j < headers.length; j++) {
+                contents[j].style.overflow = 'hidden';
+                contents[j].style.maxHeight = '0px';
+
                 headers[j].addEventListener('click', () => {
                     const modeStatement = Array.isArray(mode) ? mode[i] : mode;
                     
-                    this.attachHeaderEvent(headers[j], modeStatement);
+                    this.toggleAccordium(contents[j], modeStatement);
                 });
             }
         }
+    }
+
+    runMultiple(content) {
+        const isClose = content.style.maxHeight === '0px';
+
+        if(isClose) {
+            content.style.maxHeight = `${content.scrollHeight}px`;
+
+            return
+        }
+
+        content.style.maxHeight = '0px';
+    }
+
+    runSingle(content) {
+        console.log(content);
     }
 
     getCurrentHeader(index) {
@@ -35,12 +55,38 @@ class Accordium {
         return this.contents[index];
     }
 
-    attachHeaderEvent(element, mode) {
-        console.log({element, mode});
+    toggleAccordium(content, mode) {
+        switch (mode) {
+            case 'nested':
+                this.runSingle(content);
+                break;
+
+            case 'single':
+                console.log({ content, mode });
+                break;
+                
+            default:
+                this.runMultiple(content);
+                break;
+        }
     }
 
-    toggleAccordion(index) {
-        console.log(index);
+    findFirstParent (element, target) {
+        while (element.parentElement) {
+            if (element.parentElement.hasAttribute(target)) {
+                return element.parentElement;
+            }
+            element = element.parentElement;
+        }
+    
+        return null;
+    }
+    
+    removeFirstParent(arr, value) {
+        let index = arr.indexOf(value);
+        if (index > -1) {
+            arr.splice(index, 1);
+        }
     }
 }
 
