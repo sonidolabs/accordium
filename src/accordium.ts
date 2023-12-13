03a3a8fe5f1c262c5d90b402ee6ab7389af531f3
Gabriel <gabriel.batista@iesde.com.br>
2023-12-13 16:55:35 -0300

    feat: create nested accordion script

--- src/accordium.ts
+++ src/accordium.ts
@@ -3,212 +3,146 @@ interface Options {
     customClass: string;
 }
 
-const map = {
-    0: { // data-accordion
-        0: {
-            body: '[data-accordion-body]',
-            header: '[data-accordion-header]',
-            content: '[data-accordion-content]'
-        },
-        1: {
-            body: '[data-accordion-body]',
-            header: '[data-accordion-header]',
-            content: '[data-accordion-content]'
-        }
-    },
-    1: { // data-accordion
-        0: {
-            body: '[data-accordion-body]',
-            header: '[data-accordion-header]',
-            content: '[data-accordion-content]',
-            children: {
-                body: '[data-accordion-body]',
-                header: '[data-accordion-header]',
-                content: '[data-accordion-content]',
-                children: {
-                    body: '[data-accordion-body]',
-                    header: '[data-accordion-header]',
-                    content: '[data-accordion-content]'
-                }
-            }
-        },
-        1: {
-            body: '[data-accordion-body]',
-            header: '[data-accordion-header]',
-            content: '[data-accordion-content]',
-            children: {
-                body: '[data-accordion-body]',
-                header: '[data-accordion-header]',
-                content: '[data-accordion-content]',
-                children: {
-                    body: '[data-accordion-body]',
-                    header: '[data-accordion-header]',
-                    content: '[data-accordion-content]'
-                }
-            }
-        }
-    }
-}
-
 class Accordium {
-    accordions?: HTMLElement[] | undefined;
-    // defaultOptions: Options;
-    // options: Options;
+    options: Options;
+    defaultOptions: Options;
+    accordions: NodeListOf<HTMLElement>
+    accordiumMap: { 
+        [index: number]: { 
+            [index: number]: HTMLElement 
+        } 
+    }
 
     constructor(options = {}) {
-        const accortions = document.querySelectorAll('[data-accordion]');
-        const teste = this.createAccordiumMap(accortions);
+        this.defaultOptions = {
+            mode: ['multiple', 'single', 'nested'],
+            customClass: 'active'
+        }
 
-        // this.accordions = Array.from(document.querySelectorAll('[data-accordion]'));
+        this.options = Object.assign(this.defaultOptions, options);
+        this.accordions = document.querySelectorAll('[data-accordion]');
+        this.accordiumMap = {};
 
-        // this.defaultOptions = {
-        //     mode: 'multiple',
-        //     customClass: 'active'
-        // }
-        // this.options = Object.assign(this.defaultOptions, options);
+        for (let i = 0; i < this.accordions.length; i++) {
+            const parentIndex: number = i;
+            const headerElements: NodeListOf<HTMLElement> = this.accordions[i].querySelectorAll('[data-accordion-header]');
+            const contentElements: NodeListOf<HTMLElement> = this.accordions[i].querySelectorAll('[data-accordion-content]');
 
-        // if (this.accordions === undefined) return;
+            this.accordiumMap[i] = {};
 
-        // const { mode } = this.options;
-
-        // for (let i = 0; i < this.accordions.length; i++) {
-        //     const modeStatement: string | string[] = Array.isArray(mode) ? mode[i] : mode;
-
-        //     for (let j = 0; j < this.headers.length; j++) {
-        //         this.headers[j].innerText = `${j} - src`;
-        //         this.contents[j].style.maxHeight = "0px";
-        //         this.contents[j].style.overflow = "hidden";
-
-        //         this.headers[j].addEventListener('click', () => {
-        //             this.toggleAccordium(j, modeStatement);
-        //         });
-        //     }
-        // }
-        
-        // this.init();
-    }
-
-    createAccordiumMap(element: any) {
-        
-    }
-
-    // init(): void {
-    //     if (this.accordions === undefined) return;
-        
-    //     const { mode } = this.options;
-
-    //     for (let i = 0; i < this.accordions.length; i++) {
-    //         const headers: NodeListOf<HTMLElement> = this.accordions[i].querySelectorAll('[data-accordion-header]');
-    //         const contents: NodeListOf<HTMLElement> = this.accordions[i].querySelectorAll('[data-accordion-content]');
-            
-    //         for (let j = 0; j < headers.length; j++) {
-    //             headers[j].innerText = `${j} - src`  
-    //             // contents[j].style.maxHeight = "0px";
-    //             contents[j].style.overflow = "hidden";
-
-    //             headers[j].addEventListener('click', () => {
-    //                 const modeStatement: string | string[] = Array.isArray(mode) ? mode[i] : mode;
-                    
-    //                 this.toggleAccordium(j, contents, modeStatement);
-    //             });
-    //         }
-    //     }
-    // }
-
-    // setContentHeight(elIndex: number, contents: NodeListOf<HTMLElement>, isClose: boolean): void {
-    //     const { customClass } = this.options;
-        
-    //     if (isClose) {
-    //         contents[elIndex].classList.add(customClass);
-    //         contents[elIndex].style.maxHeight = `${contents[elIndex].scrollHeight}px`;
-    //         return
-    //     }
-
-    //     contents[elIndex].classList.remove(customClass);
-    //     contents[elIndex].style.maxHeight = '0px';
-    // }
-
-    // isClose(element: HTMLElement): boolean {
-    //     return element.style.maxHeight === '0px';
-    // }
-
-    // runMultiple(elIndex: number): void {
-    //     // const content: HTMLElement | undefined = this.contents[elIndex];
-
-    //     // if (content && this.isClose(content)) {
-    //     //     this.setContentHeight(elIndex, content, this.isClose(content))
-    //     //     return
-    //     // }
-
-    //     // this.setContentHeight(elIndex, content, this.isClose(content))
-    // }
-
-    // runSingle(elIndex: number, contents: NodeListOf<HTMLElement>): void {
-    //     const { customClass } = this.options;
-    //     const content: HTMLElement = contents[elIndex];
-
-    //     if (this.isClose(content)){
-    //         this.setContentHeight(elIndex, contents, this.isClose(content))
-
-    //         for (let i = 0; i < contents.length; i++) {
-    //             if(i != elIndex){
-    //                 contents[i].classList.remove(customClass);
-    //                 contents[i].style.maxHeight = '0px';
-    //             }
-    //         }
-    //         return
-    //     }
-
-    //     this.setContentHeight(elIndex, contents, this.isClose(content))
-    // }
-
-    // runNested(elIndex: number, contents: NodeListOf<HTMLElement>): void {
-    //     const content: HTMLElement = contents[elIndex];
-    //     const contentsParent: Map<number, HTMLElement> = new Map();
-    //     const firstContentParent: HTMLElement | null = this.findFirstParent(content, 'data-accordion-content');
-
-    //     this.setContentHeight(elIndex, contents, this.isClose(content));
-
-    //     if (firstContentParent && contentsParent.has(elIndex)) contentsParent.set(elIndex, firstContentParent);
-
-        
-    // }
-
-    // findFirstParent (element: HTMLElement | undefined, target: string): HTMLElement | null {
-    //     if (element) {
-    //         while (element.parentElement) {
-    //             if (element.parentElement.hasAttribute(target)) {
-    //                 return element.parentElement;
-    //             }
-    //             element = element.parentElement;
-    //         }
-    //     }
-
-    //     return null
-    // }
-    
-    // removeFirstParent(contentParents: HTMLElement[], content: HTMLElement) {
-    //     const index = contentParents.indexOf(content);
-    //     if (index > -1) {
-    //         contentParents.splice(index, 1);
-    //     }
-    // }
-
-    // toggleAccordium(elIndex: number, mode: string | string[]): void {
-    //     switch (mode) {
-    //         case 'nested':
-    //             this.runNested(elIndex);
-    //             break;
-
-    //         case 'single':
-    //             this.runSingle(elIndex);
-    //             break;
+            for (let j = 0; j < contentElements.length; j++) {
+                headerElements[j].innerText = `${j} - src`;
+                headerElements[j].setAttribute('id', `accordium-${j}`);
                 
-    //         default:
-    //             this.runMultiple(elIndex);
-    //             break;
-    //     }
-    // }
+                contentElements[j].style.maxHeight = '0px';
+                contentElements[j].style.overflow = 'hidden';
+
+                this.accordiumMap[i][j] = contentElements[j];
+            }
+
+            this.accordions[i].addEventListener('click', (e: MouseEvent) => {
+                const target = e.target as HTMLElement;
+
+                if (!target.hasAttribute('data-accordion-header')) return;
+                
+                const { mode } = this.options;
+                const modeState = Array.isArray(mode) ? mode[i] : mode;
+                const headerElement = e.target as HTMLElement;
+                const headerIndex: number = this.parseElementId(headerElement);
+
+                this.toggleAccordium(parentIndex, headerIndex, modeState);
+            });
+        }
+    }
+
+    parseElementId(element: HTMLElement): number {
+        return parseInt(element.id.replace('accordium-', ''));
+    }
+
+    findFirstParent (element: HTMLElement | undefined, targetAttribute: string): HTMLElement | null {
+        if (element) {
+            while (element.parentElement) {
+                if (element.parentElement.hasAttribute(targetAttribute)) {
+                    return element.parentElement;
+                }
+                element = element.parentElement;
+            }
+        }
+
+        return null
+    }
+
+    setContentHeight(content: HTMLElement, shouldClose: boolean): void {
+        const { customClass } = this.options;
+
+        if (shouldClose) {
+            content.classList.add(customClass);
+            content.style.maxHeight = `${content.scrollHeight}px`;
+
+            return
+        }
+
+        content.classList.remove(customClass);
+        content.style.maxHeight = '0px';
+    }
+
+    isClose(element: HTMLElement): boolean {
+        return element.style.maxHeight === '0px';
+    }
+
+    runMultiple(parentIndex: number, elIndex: number): void {
+        const contentElement = this.accordiumMap[parentIndex][elIndex] as HTMLElement;
+        const shouldClose = this.isClose(contentElement);
+        
+        this.setContentHeight(contentElement, shouldClose);
+    }
+
+    runSingle(parentIndex: number, elIndex: number): void {
+        const contentElement = this.accordiumMap[parentIndex][elIndex] as HTMLElement;
+        const shouldClose = this.isClose(contentElement);
+
+        this.setContentHeight(contentElement, shouldClose);
+        
+        Object.entries(this.accordiumMap[parentIndex]).forEach(([key, value]) => {
+            if (parseInt(key) !== elIndex) {
+                this.setContentHeight(value, false);
+            }
+        });
+    }
+
+    runNested(parentIndex: number, elIndex: number): void {
+        const contentElement = this.accordiumMap[parentIndex][elIndex] as HTMLElement;
+        const shouldClose = this.isClose(contentElement);
+        const currentElementHeight = contentElement.scrollHeight;
+
+        this.setContentHeight(contentElement, shouldClose);
+
+        let parentElement = this.findFirstParent(contentElement, 'data-accordion-content');        
+
+        while (parentElement) {
+            const parentElementHeight = parentElement?.scrollHeight;
+
+            parentElement.style.maxHeight = `${parentElementHeight + currentElementHeight}px`;
+    
+            parentElement = this.findFirstParent(parentElement, 'data-accordion-content');
+        }
+    }
+
+    toggleAccordium(parentIndex: number, elIndex: number, mode: string | string[]): void {
+        switch (mode) {
+            case 'nested':
+                this.runNested(parentIndex, elIndex);
+                break;
+
+            case 'single':
+                this.runSingle(parentIndex, elIndex);
+                break;
+
+            default:
+                this.runMultiple(parentIndex, elIndex);
+                break;
+        }
+    }
 }
 
 new Accordium();
